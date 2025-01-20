7629d4b2ca70b6bca7c174faee2e054939a90c22
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-01-20 15:40:42 -0300

    create.js contructing

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,7 +1,11 @@
+import chalk from "chalk";
 import handle from "./handle.js";
 
 async function createPassord() {
-    handle();
+    const password = await handle();
+    console.log(chalk.green("Your password was created!!!"));
+    console.log(`Password: ${password}`);
+
 }
 
 export default createPassord;
--- src/services/password/handle.js
+++ src/services/password/handle.js
@@ -1,16 +1,19 @@
 async function permittedCharacters() {
-    let permitedCharacters = [];
+    let permited = [];
 
     if(process.env.UPPERCASE_LETTERS === "true")
-        permitedCharacters.push(... "ABCDEFGHIJKLMNOPQRSTUVWXYX");
-    if(process.env.LOWERCASE_LETTERS === "true")
-        permitedCharacters.push(... "abcdefghijklmnopqrstuvwxyz");
-    if(process.env.NUMBERS === "true")
-        permitedCharacters.push(... "123456789");
-    if(process.env.SPECIAL_CHARACTERS === "true")
-        permitedCharacters.push(... "@#$_-");
+        permited.push(... "ABCDEFGHIJKLMNOPQRSTUVWXYX");
 
-    return permitedCharacters;
+    if(process.env.LOWERCASE_LETTERS === "true")
+        permited.push(... "abcdefghijklmnopqrstuvwxyz");
+
+    if(process.env.NUMBERS === "true")
+        permited.push(... "123456789");
+
+    if(process.env.SPECIAL_CHARACTERS === "true")
+        permited.push(... "@#$_-");
+
+    return permited;
 }
 
 
